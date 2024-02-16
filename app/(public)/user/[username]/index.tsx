4f34b9481d6b9c5c6bfacd644a8a1a8cc5c95a7d
Jordan <jordanwatson000@gmail.com>
2024-02-16 14:58:10 +0000

    error fix for colon

--- app/(public)/user/[username]/index.tsx
+++ app/(public)/user/[username]/index.tsx
@@ -1,15 +1,15 @@
 import { Text, View } from "react-native";
 import { UserContext } from "../../../../contexts/UserContent";
 import { useContext } from "react";
-import { useGlobalSearchParams } from "expo-router";
+import { useGlobalSearchParams, useLocalSearchParams } from "expo-router";
 
 const UserPage = () => {
   const { user } = useContext(UserContext);
-  const { username } = useGlobalSearchParams();
+  const { username } = useLocalSearchParams();
 
   return (
     <View>
-      <Text>Username: {username}</Text>;
+      <Text>Username: {username}</Text>
       <Text>
         Logged in user: {user.username == username ? "True" : "False"}
       </Text>
